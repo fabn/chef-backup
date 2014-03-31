77e6c0448cdbbd5f6f51843b29ed9b98b4d3fe28
Fabio Napoleoni <f.napoleoni@gmail.com>
2014-03-31 19:49:52 +0200

    Setup a duply profile with a integration test only recipe

---
+++ recipes/_integration.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook Name:: backup
+# Recipe:: _integration
+#
+# Copyright (C) 2014 Fabio Napoleoni
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Private recipe used only in integration tests
+
+# Configure backups for an hypotetical web root
+duply_profile 'homes' do
+  source '/home'
+  target "#{node[:duplicity][:defaults][:target]}_home_folders"
+  encryption_password node[:duplicity][:defaults][:encryption_password]
+end
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -27,7 +27,7 @@ describe 'Backups' do
   end
 
   describe 'Backups execution' do
-    # Full filesystem backup is skipped to avoid long running time, see filesystem_spec.rb for a smaller example
+    # Full filesystem backup is skipped to avoid long running time, see other tests for a smaller example
     describe command('HOME=/root duply mysql_zrm_incremental backup_purge_purge-full --force') do
       it { should return_exit_status(0) }
     end
